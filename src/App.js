1bdcae2bd93d626c2ab11d72cca92d70332faf2a
Hiemstra <harald.hiemstra@capgemini.com>
2023-03-10 11:55:06 +0100

    change name

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <h1>Portfolio!</h1>
+        <h1>Change Name!</h1>
       </header>
     </div>
   );
